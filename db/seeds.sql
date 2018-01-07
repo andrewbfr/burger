82e284c6c9270fb8ae14148b76f19eaa8ad997a1
Andrew Ferrer <AndBFer@Andrews-MBP.domain>
2018-01-06 18:23:39 -0700

    periodic, setting routes and methods

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,9 @@
 INSERT INTO burgers (burger_name, devoured) VALUES ("peanut butter bacon burger", "0");
 INSERT INTO burgers (burger_name, devoured) VALUES ("blueberry aioli squash with mushroom", "0");
 INSERT INTO burgers (burger_name, devoured) VALUES ("burnt toast", "0");
+INSERT INTO burgers (burger_name, devoured) VALUES ("sweet potato veggie burger", "0");
+INSERT INTO burgers (burger_name, devoured) VALUES ("chili cheese fry burger", "0");
+INSERT INTO burgers (burger_name, devoured) VALUES ("mac n' cheese", "0");
+INSERT INTO burgers (burger_name, devoured) VALUES ("guac burger", "0");
+INSERT INTO burgers (burger_name, devoured) VALUES ("bison burger", "0");
+INSERT INTO burgers (burger_name, devoured) VALUES ("turkey burger", "0");
---
+++ models/burger.js
@@ -0,0 +1,31 @@
+var orm = require("../config/orm.js");
+
+// Import the ORM to create functions that will interact with the database.
+var orm = require("../config/orm.js");
+
+var burger = {
+  selectall: function(cb) {
+    orm.all("cats", function(res) {
+      cb(res);
+    });
+  },
+  // The variables cols and vals are arrays.
+  insertOne: function(cols, vals, cb) {
+    orm.create("cats", cols, vals, function(res) {
+      cb(res);
+    });
+  },
+  updateOne: function(objColVals, condition, cb) {
+    orm.update("cats", objColVals, condition, function(res) {
+      cb(res);
+    });
+  },
+  delete: function(condition, cb) {
+    orm.delete("cats", condition, function(res) {
+      cb(res);
+    });
+  }
+};
+
+// Export the database functions for the controller (catsController.js).
+module.exports = burger;
